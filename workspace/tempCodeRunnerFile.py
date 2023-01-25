9c0c2e95618cf8097ba4355e1873daffb8bf3a2f
The1Re <kanesh132@gmail.com>
2023-01-25 12:27:39 +0700

    n

---
+++ workspace/tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+a = int(input())
+b = a+543
+an = " ".join(str(a)).split()
+bn = " ".join(str(b)).split()
+print(b)
+print("".join(an))
+print("".join(bn))
--- workspace/test.py
+++ workspace/test.py
@@ -1,15 +1,5 @@
-n = 1
-while (n!=3):
-    print ("(1) Compute A + B ")
-    print("(2) Compute A - B ")
-    print("Exit")
-    n = int(input("Enter choice :"))
-    if (n==2):
-        x,y = map(int,input("Enter 2 numbers : ").split())
-        z = x-y
-        print(x,"-",y,"=",z)
-    elif(n==1):
-        x,y = map(int,input("Enter 2 numbers : ").split())
-        z = x+y
-        print(x,"+",y,"=",z)
-    
+a = int(input())
+b = a+543
+print(b)
+print(sum(map(int," ".join(str(a)).split())))
+print(sum(map(int," ".join(str(b)).split())))
