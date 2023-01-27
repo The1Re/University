d6ecb84835e86829037c8dc8990c23c98c362fea
The1Re <119749560+The1Re@users.noreply.github.com>
2023-01-27 21:10:54 +0700

    textweb

--- workspace/test.py
+++ workspace/test.py
@@ -1,9 +1,2 @@
-x = 1
-y = 1
-z = 0
-for i in range(1,10):
-    z = x+y
-    x = y+1
-    if x%3 != 0:
-        y = 2*x
-    print(f'{i} : \t{x}\t\t{y}\t\t{z}')
+for i in range(5):
+    print('Hello world!')
