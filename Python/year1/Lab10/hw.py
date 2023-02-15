cc93cc5858c3b8160b3b866c23104cc52a952d83
The1Re <kanesh132@gmail.com>
2023-02-15 17:32:56 +0700

    hw 10

---
+++ Python/year1/Lab10/hw.py
@@ -0,0 +1,26 @@
+def hw1():
+    data = input('Input : ').split('_')
+    print('First Name :',data[0])
+    print('Middle Name :',data[1])
+    print('Last Name :',data[2])
+    print(type(data))
+
+def hw2():
+    msg = input('Enter string : ')
+    print('string : ',end='')
+    for i in range(len(msg)):
+        if msg[i] == ' ':
+            print('-',end='')
+        else:
+            print(msg[i],end='')
+
+def hw3():
+    a = input('Enter string A : ')
+    b = input('Enter string B : ')
+    pos = a.find(b)
+    if pos != -1:
+        print(b+' is contained in '+a+' at position',pos,'to',pos+len(b))
+    else:
+        print(b+' is not contained in '+a)
+
+hw1()
--- Python/year1/Lab8/hw.py
+++ Python/year1/Lab8/hw.py
@@ -1,26 +1,5 @@
-def hw1():
-    x = [1,2,3,4,5,6]
-    for i in range(int(len(x)/2)):
-        t = x[i]
-        x[i] = x[len(x)-i-1]
-        x[len(x)-i-1] = t
-    print(x)
-
-def hw2():
-    binary = list(map(int,input('Enter 8 binary number (0,1) : ').split()))
-    dec = 0
-    for i in range(8):
-        dec += binary[i]*2**(7-i)
-    print('The decimal number is',dec)
-
-def hw2_2():
-    binary = []
-    binary = input('Input 8 binary numbers (0 or 1) : ').split()
-    for i in range(len(binary)):
-        binary[i] = int(binary[i])
-    dec = 0
-    for i in range(len(binary)):
-        dec += binary[i]*2**(len(binary)-i-1)
-    print('The decimal number is',dec)
-
-hw2_2()
+binary = list(map(int,input('Enter 8 binary number (0,1) : ').split()))
+dec = 0
+for i in range(8):
+    dec += binary[i]*2**(7-i)
+print('The decimal number is',dec)
